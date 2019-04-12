155e9ada098885f0bf7042010fd2bd875ffc1272
skimura1101 <skimura@sc.ctrl.titech.ac.jp>
2019-04-12 11:42:39 +0900

    deleated files

--- installforMac.sh
+++
@@ -1,6 +0,0 @@
-ln -fsn ~/.dotfiles/.zshrcMac ~/.zshrc
-ln -fsn ~/.dotfiles/.vim ~/.vim
-ln -fsn ~/.dotfiles/.vimrc ~/.vimrc
-ln -fsn ~/.dotfiles/.gvimrc ~/.gvimrc
-ln -fsn ~/.dotfiles/.latexmkrc ~/.latexmkrc
-
